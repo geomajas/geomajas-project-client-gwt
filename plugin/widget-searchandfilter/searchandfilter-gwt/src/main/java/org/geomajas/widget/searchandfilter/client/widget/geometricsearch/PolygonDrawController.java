850c4b5c3575b776edadb2d82a8ccd2071f7c0fe
janv <jan.venstermans@dfc.be>
2014-04-01 19:26:12 +0200

    GWT-559:  in search and filter: spatial search : chrome : polygon creation can be finished by double click. Essentially PolygonDrawController refactored so it extends the LineStringDrawController.

--- plugin/widget-searchandfilter/searchandfilter-gwt/src/main/java/org/geomajas/widget/searchandfilter/client/widget/geometricsearch/PolygonDrawController.java
+++ plugin/widget-searchandfilter/searchandfilter-gwt/src/main/java/org/geomajas/widget/searchandfilter/client/widget/geometricsearch/PolygonDrawController.java
@@ -10,8 +10,8 @@
  */
 package org.geomajas.widget.searchandfilter.client.widget.geometricsearch;
 
+import com.google.gwt.event.dom.client.MouseEvent;
 import org.geomajas.geometry.Coordinate;
-import org.geomajas.gwt.client.controller.editing.EditController.EditMode;
 import org.geomajas.gwt.client.gfx.paintable.GfxGeometry;
 import org.geomajas.gwt.client.gfx.style.ShapeStyle;
 import org.geomajas.gwt.client.spatial.geometry.GeometryFactory;
@@ -22,25 +22,19 @@ import org.geomajas.gwt.client.widget.MapWidget;
 import org.geomajas.gwt.client.widget.MapWidget.RenderGroup;
 import org.geomajas.gwt.client.widget.MapWidget.RenderStatus;
 
-import com.google.gwt.event.dom.client.DoubleClickEvent;
-import com.google.gwt.event.dom.client.MouseDownEvent;
-import com.google.gwt.event.dom.client.MouseEvent;
-import com.google.gwt.event.dom.client.MouseMoveEvent;
-import com.google.gwt.event.dom.client.MouseUpEvent;
-import com.google.gwt.user.client.Event;
-
 /**
  * Drawing controller for Polygon geometries.
  * 
  * @author Bruce Palmkoeck
+ * @author Jan Venstermans
  */
-public class PolygonDrawController extends AbstractFreeDrawingController {
+public class PolygonDrawController extends LineStringDrawController {
 
-	private GfxGeometry tempLine1;
-
-	private GfxGeometry tempLine2;
-
-	private GfxGeometry tempLineEnd;
+	/**
+	 * visualisation of currently saved polygon (after last click),
+	 * without the extra line between last and first point.
+	 */
+	private GfxGeometry tempPolygon;
 
 	private ShapeStyle drawStyleGood = new ShapeStyle("#FF7F00", 0.3f, "#FF7F00", 1, 2);
 
@@ -50,130 +44,82 @@ public class PolygonDrawController extends AbstractFreeDrawingController {
 	// Constructor:
 	// -------------------------------------------------------------------------
 
-	public PolygonDrawController(MapWidget mapWidget, AbstractFreeDrawingController parent, 
-			GeometryDrawHandler handler) {
+	public PolygonDrawController(MapWidget mapWidget, AbstractFreeDrawingController parent,
+								 GeometryDrawHandler handler) {
 		super(mapWidget, parent, handler);
-		factory = new GeometryFactory(mapWidget.getMapModel().getSrid(), mapWidget.getMapModel().getPrecision());
+		// overwrite created geometry in superclass
 		geometry = factory.createPolygon(null, null);
 	}
 
 	// -------------------------------------------------------------------------
-	// DrawController implementation:
+	// Protected methods:
 	// -------------------------------------------------------------------------
 
-	public void cleanup() {
-		removeTempAll();
-	}
+	@Override
+	protected void addCoordinateToGeometry(Coordinate newCoordinate) {
+		boolean geometryHasCoordinates = geometry.getCoordinates() != null;
 
-	public boolean isBusy() {
-		// busy when inserting or dragging has started
-		return getEditMode() == EditMode.INSERT_MODE || getEditMode() == EditMode.DRAG_MODE;
-	}
-
-	// -------------------------------------------------------------------------
-	// MapController implementation:
-	// -------------------------------------------------------------------------
-
-	public void onMouseDown(MouseDownEvent event) {
-		if (event.getNativeButton() != Event.BUTTON_RIGHT) {
-			createTempLine();
+		// determine length and coordinates
+		int length = geometryHasCoordinates ? geometry.getCoordinates().length : 0;
+		Coordinate[] oldCoords = geometryHasCoordinates ? geometry.getCoordinates() : null;
+		Coordinate[] newCoords = new Coordinate[length + 1];
+		if (geometryHasCoordinates) {
+			System.arraycopy(oldCoords, 0, newCoords, 0, length);
 		}
-	}
 
-	public void onMouseMove(MouseMoveEvent event) {
-		updateTempLine(event);
-	}
-
-	public void onMouseUp(MouseUpEvent event) {
-		if (event.getNativeButton() != Event.BUTTON_RIGHT) {
-
-			Coordinate[] oldCoords = null;
-			Coordinate[] newCoords;
-			int length = 0;
-
-			if (geometry.getCoordinates() != null) {
-				length = geometry.getCoordinates().length;
-				oldCoords = geometry.getCoordinates();
-			}
-			newCoords = new Coordinate[length + 1];
-
-			if (oldCoords != null) {
-				System.arraycopy(oldCoords, 0, newCoords, 0, length);
-			}
-
-			LinearRing linearRing;
-
-			if (newCoords.length < 2) {
-				newCoords[length] = getWorldPosition(event);
-			} else {
-				newCoords[length] = newCoords[length - 1];
-				newCoords[length - 1] = getWorldPosition(event);
-			}
-
-			linearRing = factory.createLinearRing(newCoords);
-			geometry = factory.createPolygon(linearRing, null);
-
-			Coordinate[] srCoords = new Coordinate[geometry.getCoordinates().length];
-			Coordinate[] worldCoords = geometry.getCoordinates();
-			for (int i = 0; i < srCoords.length; i++) {
-				srCoords[i] = getTransformer().worldToPan(worldCoords[i]);
-			}
-
-			tempLineEnd = new GfxGeometry("LineStringEditController.updateLineEnd");
-			tempLineEnd.setStyle(new ShapeStyle("#FF3322", 0.5f, "#FF3322", 1, 1));
-
-			linearRing = geometry.getGeometryFactory().createLinearRing(srCoords);
-			Polygon polygon = geometry.getGeometryFactory().createPolygon(linearRing, null);
-
-			if (geometry.isValid()) {
-				tempLineEnd.setStyle(drawStyleGood);
-			} else {
-				tempLineEnd.setStyle(drawStyleBad);
-			}
-
-			tempLineEnd.setGeometry(polygon);
-			mapWidget.render(tempLineEnd, RenderGroup.VECTOR, RenderStatus.ALL);
-			removeTempLines();
+		// put new coordinate in right position
+		if (newCoords.length < 2) {
+			newCoords[length] = newCoordinate;
+		} else {
+			newCoords[length] = newCoords[length - 1];
+			newCoords[length - 1] = newCoordinate;
 		}
+
+		// create new geometry
+		LinearRing linearRing = factory.createLinearRing(newCoords);
+		geometry = factory.createPolygon(linearRing, null);
+
+		// update drawing
+		updateTempLineAfterClick();
 	}
 
 	@Override
-	public void onDoubleClick(DoubleClickEvent event) {
-		if (event.getNativeButton() != Event.BUTTON_RIGHT) {
-			handler.onDraw(geometry);
+	protected void updateTempLineAfterClick() {
+		if (tempPolygon == null) {
+			tempPolygon = new GfxGeometry("LineStringEditController.updatePolygon");
+			tempLine = new GfxGeometry("LineStringEditController.updateLine");
 		}
+
+		if (geometry.isValid()) {
+			tempPolygon.setStyle(drawStyleGood);
+			tempLine.setStyle(drawStyleGood);
+		} else {
+			tempPolygon.setStyle(drawStyleBad);
+			tempLine.setStyle(drawStyleBad);
+		}
+
+		Coordinate[] worldCoords = geometry.getCoordinates() != null ? geometry.getCoordinates() : new Coordinate[0];
+		int length = worldCoords != null ? worldCoords.length : 0;
+		// remove the connection line between last and first point
+		if (length > 2 && worldCoords[0].equals(worldCoords[length - 1])) {
+			length--;
+		}
+		Coordinate[] srCoords = new Coordinate[length];
+		for (int i = 0; i < srCoords.length; i++) {
+			srCoords[i] = getTransformer().worldToPan(worldCoords[i]);
+		}
+
+		GeometryFactory geometryFactory = geometry.getGeometryFactory();
+		LinearRing linearRing = geometryFactory.createLinearRing(srCoords);
+		Polygon polygon = geometryFactory.createPolygon(linearRing, null);
+		tempPolygon.setGeometry(polygon);
+		mapWidget.render(tempPolygon, RenderGroup.VECTOR, RenderStatus.UPDATE);
 	}
 
-	// Getters and setters:
-
-	public void setEditMode(EditMode editMode) {
-		super.setEditMode(editMode);
-		if (editMode == EditMode.DRAG_MODE) {
-			removeTempLines();
-		}
-	}
-
-	// Private methods:
-
-	private void createTempLine() {
-		if (tempLine1 == null && geometry instanceof Polygon) {
-			tempLine1 = new GfxGeometry("LineStringEditController.updateLine1");
-			tempLine2 = new GfxGeometry("LineStringEditController.updateLine2");
-
-			if (geometry.isValid()) {
-				tempLine1.setStyle(drawStyleGood);
-				tempLine2.setStyle(drawStyleGood);
-
-			} else {
-				tempLine1.setStyle(drawStyleBad);
-				tempLine2.setStyle(drawStyleBad);
-			}
-		}
-	}
-
-	private void updateTempLine(MouseEvent<?> event) {
-		if (tempLine1 == null) {
-			createTempLine();
+	@Override
+	protected void updateTempLineAfterMove(MouseEvent<?> event) {
+		if (tempPolygon == null) {
+			updateTempLineAfterClick();
 		}
 
 		Polygon polygon = (Polygon) geometry;
@@ -187,43 +133,23 @@ public class PolygonDrawController extends AbstractFreeDrawingController {
 				Coordinate firstCoordinate = coordinates[0];
 				Coordinate lastCoordinate = coordinates[coordinates.length - 2];
 
-				LineString lineString1 = geometry.getGeometryFactory().createLineString(
-						new Coordinate[] { getTransformer().worldToPan(lastCoordinate), getPanPosition(event) });
-				tempLine1.setGeometry(lineString1);
+				LineString lineStringPan = geometry.getGeometryFactory().createLineString(new Coordinate[] {
+								getTransformer().worldToPan(lastCoordinate),
+								getPanPosition(event),
+								getTransformer().worldToPan(firstCoordinate) });
+				tempLine.setGeometry(lineStringPan);
 
-				LineString lineString2 = geometry.getGeometryFactory().createLineString(
-						new Coordinate[] { getTransformer().worldToPan(firstCoordinate), getPanPosition(event) });
-				tempLine2.setGeometry(lineString2);
-
-				mapWidget.render(tempLine1, RenderGroup.VECTOR, RenderStatus.ALL);
-				mapWidget.render(tempLine2, RenderGroup.VECTOR, RenderStatus.ALL);
+				mapWidget.render(tempLine, RenderGroup.VECTOR, RenderStatus.ALL);
 			}
 		}
 	}
 
-	private void removeTempLines() {
-		if (tempLine1 != null) {
-			mapWidget.render(tempLine1, RenderGroup.VECTOR, RenderStatus.DELETE);
-			tempLine1 = null;
+	@Override
+	protected void removeTempLine() {
+		if (tempPolygon != null) {
+			mapWidget.render(tempPolygon, RenderGroup.VECTOR, RenderStatus.DELETE);
+			tempPolygon = null;
 		}
-		if (tempLine2 != null) {
-			mapWidget.render(tempLine2, RenderGroup.VECTOR, RenderStatus.DELETE);
-			tempLine2 = null;
-		}
-	}
-
-	private void removeTempAll() {
-		if (tempLine1 != null) {
-			mapWidget.render(tempLine1, RenderGroup.VECTOR, RenderStatus.DELETE);
-			tempLine1 = null;
-		}
-		if (tempLine2 != null) {
-			mapWidget.render(tempLine2, RenderGroup.VECTOR, RenderStatus.DELETE);
-			tempLine2 = null;
-		}
-		if (tempLineEnd != null) {
-			mapWidget.render(tempLineEnd, RenderGroup.VECTOR, RenderStatus.DELETE);
-			tempLineEnd = null;
-		}
+		super.removeTempLine();
 	}
 }
